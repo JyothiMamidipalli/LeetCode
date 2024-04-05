f2fb4c5b798cda8ebea8300a2b8f06430d887983
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-04-05 23:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (49.20%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
+    bool jyo(char ch1,char ch2)
+    {
+        if(ch1>='a' and ch1<='z')
+        {
+            ch1=char(int(ch1)-int('a')+int('A'));
+        }
+        else
+        {
+            ch1=char(int(ch1)-int('A')+int('a'));
+        }
+        return ch1==ch2;
+    }
     string makeGood(string s) {
         ios_base::sync_with_stdio(0);
         cin.tie(nullptr);
         int n=s.length();
         if(n==0 or n==1) return s;
-        string temp;
-        int i=0;
         int cnt=0;
+        int i=0;
+        stack<char>j;
         while(i<n)
         {
-            if(s[i]>='A' and s[i]<='Z' and s[i+1]>='a' and s[i+1]<='z' and s[i]==toupper(s[i+1]))
+            if(j.empty())
             {
-                i+=2;
-                cnt++;
+                j.push(s[i]);
             }
-            else if(s[i]>='a' and s[i]<='z' and s[i+1]>='A' and s[i+1]<='Z' and s[i]==tolower(s[i+1]))
+            else if(jyo(j.top(),s[i]))
             {
-                i+=2;
-                cnt++;
+                j.pop();
             }
             else
             {
-                temp+=s[i];
-                i++;
+                j.push(s[i]);
             }
+            i++;
         }
-        if(cnt!=0) return makeGood(temp);
-        return temp;
+        string raj;
+        while(!j.empty())
+        {
+            raj+=j.top();
+            j.pop();
+        }
+        reverse(raj.begin(),raj.end());
+        return raj;
          
     }
 };
