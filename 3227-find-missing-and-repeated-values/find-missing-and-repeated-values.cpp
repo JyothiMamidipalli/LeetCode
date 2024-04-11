60e50c12098a9601ff3ebcaea1a57d44a1bf9526
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-04-11 22:40:04 +0530

    Time: 35 ms (33.31%) | Memory: 31.5 MB (40.44%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        vector<int>jyo;
+        map<int,int>raj;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid.size();j++)
+            {
+                raj[grid[i][j]]++;
+            }
+        }
+        for(auto it:raj)
+        {
+            if(it.second==2) jyo.push_back(it.first);
+        }
+        int n=grid.size()*grid.size();
+        for(int i=1;i<=n;i++)
+        {
+            if(raj[i]==0) jyo.push_back(i);
+        }
+        return jyo;
+    }
+};
